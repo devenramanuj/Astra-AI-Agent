6f2b1f344e81518511f132df3ee472503b51466d
devenramanuj <43030670+devenramanuj@users.noreply.github.com>
2026-07-25 16:20:39 +0530

    Update gemini_client.py

--- app/gemini_client.py
+++ app/gemini_client.py
@@ -1,6 +1,10 @@
 import os
+
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
+
+from tools import create_order
 
 load_dotenv()
 
@@ -21,13 +25,46 @@ def ask_gemini(prompt):
 
     return response.text
 
+
 def ask_gemini_with_tools(prompt, tools):
+
     response = client.models.generate_content(
         model=MODEL,
         contents=prompt,
-        config={
-            "tools": tools
-        }
+        config=types.GenerateContentConfig(
+            tools=tools
+        )
     )
+
+    # Gemini એ function call કર્યો છે?
+    if (
+        response.candidates
+        and response.candidates[0].content.parts
+        and hasattr(response.candidates[0].content.parts[0], "function_call")
+        and response.candidates[0].content.parts[0].function_call
+    ):
+
+        fc = response.candidates[0].content.parts[0].function_call
+
+        if fc.name == "create_order":
+
+            order = create_order(
+                product=fc.args["product"],
+                quantity=fc.args["quantity"],
+                price=fc.args["price"]
+            )
+
+            followup = client.models.generate_content(
+                model=MODEL,
+                contents=[
+                    prompt,
+                    types.Part.from_function_response(
+                        name="create_order",
+                        response=order
+                    )
+                ]
+            )
+
+            return followup
 
     return response
