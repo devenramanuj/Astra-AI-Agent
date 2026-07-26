9e1fe3c946d5f24831a9b3a88d46e53eef42c503
devenramanuj <43030670+devenramanuj@users.noreply.github.com>
2026-07-26 08:52:57 +0530

    Add type hints to remember function

--- app/memory.py
+++ app/memory.py
@@ -1,10 +1,12 @@
 memory = []
 
-def remember(user, assistant):
+
+def remember(user: str, assistant: str):
     memory.append({
         "user": user,
         "assistant": assistant
     })
+
 
 def get_memory():
     return memory
